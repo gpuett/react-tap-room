15c7ff5eb7011e2ba8acff4bae46e059e0f108a8
Garnett Puett <gpuett@gmail.com>
2018-06-29 09:47:28 -0700

    add masterKegList to TapList

--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -1,6 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import background from './../assets/images/blood-moon.jpg'
+
+
+const state = {
+  masterKegList: [
+    {
+      name: 'Huntsman IIPA',
+      brewer: 'Yharnam Brewing',
+      abv: '6.9%',
+      price: '5',
+      remaining: '124'
+    },
+    {
+      name: 'Cainhurst Cider',
+      brewer: 'Silver Lady Orchard',
+      abv: '5.2%',
+      price: '4',
+      remaining: '124'
+    },
+    {
+      name: 'Celestial Saisson',
+      brewer: 'Iosefka\'s Clinic',
+      abv: '4.8%',
+      price: '5',
+      remaining: '124'
+    },
+    {
+      name: 'Mod One IIPA',
+      brewer: 'Hemwick Hops',
+      abv:  '8%',
+      price: '6',
+      remaining: '124'
+    }
+  ]
+}
 
 function TapList() {
   return(

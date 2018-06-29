0a54e390422d5148ebc83e5585ee9754f909036b
Garnett Puett <gpuett@gmail.com>
2018-06-29 08:46:58 -0700

    add component structure image

--- webpack.config.js
+++ webpack.config.js
@@ -74,7 +74,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Template',
+      title: 'React Tap Room',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]

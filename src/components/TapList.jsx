bb49a346bd28696c89695a5688c02717013edd1d
Garnett Puett <gpuett@gmail.com>
2018-07-06 09:26:58 -0700

    begin lifting state

--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -1,47 +1,5 @@
 import React from 'react';
-import Celestial from './../assets/images/Celestial.jpg';
-import Huntsman from './../assets/images/huntsman.jpg';
-import SilverLady from './../assets/images/silver-lady.jpg';
-import MadOne from './../assets/images/madone.jpg';
 import Tap from './Tap';
-
-
-
-const masterKegList = [
-  {
-    name: 'Huntsman IIPA',
-    brewer: 'Yharnam Brewing',
-    abv: '6.9%',
-    price: '5',
-    image: Huntsman,
-    remaining: '124'
-  },
-  {
-    name: 'Cainhurst Cider',
-    brewer: 'Silver Lady Orchard',
-    abv: '5.2%',
-    price: '4',
-    image: SilverLady,
-    remaining: '124'
-  },
-  {
-    name: 'Celestial Saisson',
-    brewer: 'Iosefka\'s Clinic',
-    abv: '4.8%',
-    price: '5',
-    image: Celestial,
-    remaining: '124'
-  },
-  {
-    name: 'Mad One IIPA',
-    brewer: 'Hemwick Hops',
-    abv:  '8%',
-    price: '6',
-    image: MadOne,
-    remaining: '124'
-  }
-];
-
 
 function TapList() {
   return(
@@ -60,7 +18,7 @@ function TapList() {
           }
         `}
       </style>
-      {masterKegList.map((tap, index) =>
+      {masterTapList.map((tap, index) =>
         <div className='loop'>
           <Tap name={tap.name}
             brewer={tap.brewer}

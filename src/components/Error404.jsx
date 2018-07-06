966e91542ccad001a80980e907360d8ce5943897
Garnett Puett <gpuett@gmail.com>
2018-07-06 15:42:53 -0700

    fix error page styling

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -5,8 +5,23 @@ import PropTypes from 'prop-types';
 function Error404(props){
   return (
     <div>
-      <h2>The page {props.location.pathname} does not exist!</h2>
-      <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
+      <style jsx>{`
+        .error {
+          text-align: center;
+          color: white;
+          padding-top: 25vh;
+          display: flex;
+          flex-direction: column;
+        }
+        .error a {
+          color: red;
+          text-decoration: none;
+        }
+      `}</style>
+      <div className='error'>
+        <h2>The page {props.location.pathname} does not exist!</h2>
+        <h3>Would you like to return <Link to="/">home</Link> instead?</h3>
+      </div>
     </div>
   );
 }
